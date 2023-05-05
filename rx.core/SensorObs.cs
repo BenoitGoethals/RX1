2da250d0d6bfe6a01640f1b104babd4534ab651e
benoit <benoit.goethals@gmail.com>
2023-05-05 11:20:22 +0200

    update

--- rx.core/SensorObs.cs
+++ rx.core/SensorObs.cs
@@ -8,47 +8,47 @@ public class SensorObs : IObservable<Measurement>
 {
     private readonly Random _randomInt = new();
     public bool IsRunning { get; private set; }
-    private readonly List<IObserver<Measurement>>? _observers=new();
+    private readonly List<IObserver<Measurement>>? _observers = new();
     private CancellationToken _cancellationToken;
-    private readonly CancellationTokenSource _cancellationTokenSource=new CancellationTokenSource();
+    private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
     private readonly ConcurrentBag<Measurement> _measurements = new();
     private Task _task;
 
-    
+
     private async Task RunSensor()
     {
         if (IsRunning) return;
         IsRunning = true;
-        _cancellationToken=_cancellationTokenSource.Token;
-     
+        _cancellationToken = _cancellationTokenSource.Token;
+
         while (!_cancellationToken.IsCancellationRequested)
         {
-        
-                var delay = Task.Delay(500, _cancellationToken);
-                if (_observers != null)
-                    foreach (var observer in _observers.ToList())
-                    {
-                        var mes = new Measurement()
-                        {
-                            Id = Guid.NewGuid(),
-                            Temp = _randomInt.Next(60),
-                            TimeCreated = DateTime.Now,
-                            Humidity = _randomInt.Next(10, 11),
-                            WindSpeed = _randomInt.Next(0, 16)
-                        };
-                        _measurements.Add(mes);
-                        try
-                        {
-                            observer.OnNext(mes);
-                        }
-                        catch (Exception ex)
-                        {
-                            observer.OnError(ex);
-                        }
-                    }
 
-                await delay;
-            
+            var delay = Task.Delay(500, _cancellationToken);
+            if (_observers != null)
+                foreach (var observer in _observers.ToList())
+                {
+                    var mes = new Measurement()
+                    {
+                        Id = Guid.NewGuid(),
+                        Temp = _randomInt.Next(60),
+                        TimeCreated = DateTime.Now,
+                        Humidity = _randomInt.Next(10, 11),
+                        WindSpeed = _randomInt.Next(0, 16)
+                    };
+                    _measurements.Add(mes);
+                    try
+                    {
+                        observer.OnNext(mes);
+                    }
+                    catch (Exception ex)
+                    {
+                        observer.OnError(ex);
+                    }
+                }
+
+            await delay;
+
         }
 
         IsRunning = false;
@@ -58,19 +58,17 @@ public class SensorObs : IObservable<Measurement>
                 observer.OnCompleted();
             }
     }
-       
 
-    public void Start(){
 
+    public void Start()
+    {
         _task = Task.Run(async () => { await RunSensor(); }, _cancellationToken);
-
-     
     }
 
     public void Stop()
     {
         _cancellationTokenSource.Cancel();
-       
+
     }
 
 
